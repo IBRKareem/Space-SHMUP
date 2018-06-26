9cd30684128c6f18bb48a9324d196d5205ffd44b
IBRKareem <36110628+IBRKareem@users.noreply.github.com>
2018-06-26 12:22:19 -0400

    Collider Works

--- Assets/__Scripts/Hero.cs
+++ Assets/__Scripts/Hero.cs
@@ -1,89 +1,44 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using System.Collections;
 
-using System.Collections.Generic;
+public class Hero : MonoBehaviour {
 
-using UnityEngine;
-
-
-
-public class Hero : MonoBehaviour
-{
-
-	static public Hero      S; // Singleton                               // a
+	static public Hero		S;
 
 
+    [Header("Set in inspector")]
+	public float	speed = 30;
+	public float	rollMult = -45;
+	public float  	pitchMult=30;
 
-	[Header("Set in Inspector")]
-
-	// These fields control the movement of the ship
-
-	public float            speed = 30;
-
-	public float            rollMult = -45;
-
-	public float            pitchMult = 30;
+    [Header("Set dynamically")]
+	public float	shieldLevel=1;
 
 
-
-	[Header("Set Dynamically")]
-
-	public float            shieldLevel = 1; 
-
-
-
-	void Awake() {
-
-		if (S == null) {
-
-
-			S = this; // Set the Singleton                                   // a
-
-		} else {
-
-			Debug.LogError("Hero.Awake() - Attempted to assign second Hero.S!");
-
-		}
-
-	}
+    private void Awake()
+    {
+        if (S == null)
+        {
+            S = this;
+        } else
+        {
+            Debug.LogError("Hero.Awake() - Attempted to assign second Hero.S!");
+        }
+    }
 
 
-
+	
+	// Update is called once per frame
 	void Update () {
-
-		// Pull in information from the Input class
-
-		float xAxis = Input.GetAxis("Horizontal");                            // b
-
-		float yAxis = Input.GetAxis("Vertical");                              // b
-
-
-
-		// Change transform.position based on the axes
+		float xAxis = Input.GetAxis("Horizontal");
+		float yAxis = Input.GetAxis("Vertical");
 
 		Vector3 pos = transform.position;
-
 		pos.x += xAxis * speed * Time.deltaTime;
-
 		pos.y += yAxis * speed * Time.deltaTime;
-
 		transform.position = pos;
-
-
-
-		// Rotate the ship to make it feel more dynamic                      // c
-
-		transform.rotation = Quaternion.Euler(yAxis*pitchMult,xAxis*rollMult,0);
-
+		
+		// rotate the ship to make it feel more dynamic
+		transform.rotation =Quaternion.Euler(yAxis*pitchMult, xAxis*rollMult,0);
 	}
-
-	void OnTriggerEnter(Collider other) { 
-
-		Transform rootT = other.gameObject.transform.root;
-
-		GameObject go = rootT.gameObject;
-
-		print("Triggered: "+go.name);
-
-	}
-
 }
--- Assets/__Scripts/Shield.cs
+++ Assets/__Scripts/Shield.cs
@@ -1,63 +1,31 @@
-﻿using System.Collections;
-
-using System.Collections.Generic;
-
-using UnityEngine;
-
-
+﻿using UnityEngine;
+using System.Collections;
 
 public class Shield : MonoBehaviour {
 
-	[Header("Set in Inspector")]
-
-	public float    rotationsPerSecond = 0.1f;
-
-
-
-	[Header("Set Dynamically")]
-
-	public int      levelShown = 0;
+    [Header("Set in inspector")]
+	public float rotationsPerSecond = 0.1f;
+	
+    [Header("Set Dynamically")]
+	public int levelShown = 0;
 
 
-
-	// This non-public variable will not appear in the Inspector
-
-	Material        mat;                                                   // a
-
-
-
-	void Start() {
-
-		mat = GetComponent<Renderer>().material;                           // b
+	// Use this for initialization
+	void Start () {
 
 	}
-
-
-
+	
+	// Update is called once per frame
 	void Update () {
-
-		// Read the current shield level from the Hero Singleton
-
-		int currLevel = Mathf.FloorToInt( Hero.S.shieldLevel );            // c
-
-		// If this is different from levelShown...
-
-		if (levelShown != currLevel) {
-
-			levelShown = currLevel; 
-
-			// Adjust the texture offset to show different shield level
-
-			mat.mainTextureOffset = new Vector2( 0.2f*levelShown, 0 );     // d
-
+		int currlevel = Mathf.FloorToInt (Hero.S.shieldLevel);
+		if (levelShown != currlevel) {
+			levelShown = currlevel;
+			Material mat = this.GetComponent<Renderer>().material;
+			mat.mainTextureOffset = new Vector2(0.2f*levelShown, 0f);
 		}
 
-		// Rotate the shield a bit every frame in a time-based way
-
-		float rZ = -(rotationsPerSecond*Time.time*360) % 360f;             // e
-
-		transform.rotation = Quaternion.Euler( 0, 0, rZ );
+		float rZ = (rotationsPerSecond * Time.time * 360) % 360f;
+		transform.rotation = Quaternion.Euler (0, 0, rZ);
 
 	}
-
 }
