b2dd0a367da95489730c93a5427301bf6d5576da
IBRKareem <36110628+IBRKareem@users.noreply.github.com>
2018-06-26 12:37:05 -0400

    End of Ch 30
    Game is Functioning.

--- Assets/__Scripts/Hero.cs
+++ Assets/__Scripts/Hero.cs
@@ -26,6 +26,10 @@ public class Hero : MonoBehaviour
 	public float            gameRestartDelay = 2f;
 
 
+	public GameObject       projectilePrefab;
+
+	public float            projectileSpeed = 40;
+
 
 
 
@@ -85,7 +89,31 @@ public class Hero : MonoBehaviour
 
 		transform.rotation = Quaternion.Euler(yAxis*pitchMult,xAxis*rollMult,0);
 
+		// Allow the ship to fire
+
+		if ( Input.GetKeyDown( KeyCode.Space ) ) {                           // a
+
+			TempFire();
+
+		}
+
 	}
+
+
+
+	void TempFire() {                                                        // b
+
+		GameObject projGO = Instantiate<GameObject>( projectilePrefab );
+
+		projGO.transform.position = transform.position;
+
+		Rigidbody rigidB = projGO.GetComponent<Rigidbody>();
+
+		rigidB.velocity = Vector3.up * projectileSpeed;
+
+	}
+
+
 
 	void OnTriggerEnter(Collider other) {
 
