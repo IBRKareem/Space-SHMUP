d45c0ffde3f434bb20e92fa924dd5a1b728e0d73
IBRKareem <36110628+IBRKareem@users.noreply.github.com>
2018-07-03 15:22:54 -0400

    Final WIth Errors
    Ship Nolonger Shoots, Error in Enemy 4

--- Assets/__Scripts/Enemy.cs
+++ Assets/__Scripts/Enemy.cs
@@ -1,212 +1,109 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using System.Collections; // Required for Arrays & other Collections
+using System.Collections.Generic; // Required for Lists and Dictionaries
+using UnityEngine; // Required for Unity
 
 public class Enemy : MonoBehaviour {
-
 	[Header("Set in Inspector: Enemy")]
-
-	public float speed = 10f;      // The speed in m/s
-
-	public float fireRate = 0.3f;  // Seconds/shot (Unused)
-
+	public float speed = 10f; // The speed in m/s
+	public float fireRate = 0.3f; // Seconds/shot (Unused)
 	public float health = 10;
-
-	public int score = 100;      // Points earned for destroying this
-
+	public int score = 100; // Points earned for destroying this
 	public float showDamageDuration = 0.1f; // # seconds to show damage // a
-
-
+	public float powerUpDropChance = 1f; // Chance to drop a power-up // a
 
 	[Header("Set Dynamically: Enemy")]
-
 	public Color[] originalColors;
-
 	public Material[] materials;// All the Materials of this & its children
-
 	public bool showingDamage = false;
-
 	public float damageDoneTime; // Time to stop showing damage
-
 	public bool notifiedOfDestruction = false; // Will be used later
 
 
-	protected BoundsCheck bndCheck;                                            // a
+	protected BoundsCheck bndCheck;
 
-
-
-	void Awake()
-	{                                                           // b
-
+	void Awake() {
 		bndCheck = GetComponent<BoundsCheck>();
-
-		materials = Utils.GetAllMaterials(gameObject);                     // b
-
+		// Get materials and colors for this GameObject and its children
+		materials = Utils.GetAllMaterials(gameObject); // b
 		originalColors = new Color[materials.Length];
-
-		for (int i = 0; i < materials.Length; i++)
-		{
-
+		for (int i = 0; i < materials.Length; i++) {
 			originalColors[i] = materials[i].color;
-
 		}
-
 	}
 
 	// This is a Property: A method that acts like a field
-
-	public Vector3 pos
-	{                                                     // a
-
-
-		get
-		{
-
-			return (this.transform.position);
-
+	public Vector3 pos {
+		get {
+			return( this.transform.position );
 		}
-
-		set
-		{
-
+		set {
 			this.transform.position = value;
-
 		}
-
 	}
 
-
-
-	void Update()
-	{
-
+	void Update() {
 		Move();
 
-		if (showingDamage && Time.time > damageDoneTime)
-		{                 // c
-
+		if (showingDamage && Time.time > damageDoneTime) { // c
 			UnShowDamage();
-
 		}
 
-		if (bndCheck != null && bndCheck.offDown )
-		{                    // c
-
-
-
+		if (bndCheck != null && bndCheck.offDown) {
+			// We're off the bottom, so destroy this GameObject
 			Destroy(gameObject);
-
 		}
-
-
 	}
 
-
-
-	public virtual void Move()
-	{                                             // b
-
+	public virtual void Move() { // b
 		Vector3 tempPos = pos;
-
 		tempPos.y -= speed * Time.deltaTime;
-
 		pos = tempPos;
-
 	}
 
-	void OnCollisionEnter(Collision coll)
-	{                                // a
-
+	void OnCollisionEnter(Collision coll) { // a
 		GameObject otherGO = coll.gameObject;
-
-		switch (otherGO.tag)
-		{
-
-		case "ProjectileHero":                                           // b
-
+		switch (otherGO.tag) {
+		case "ProjectileHero": // b
 			Projectile p = otherGO.GetComponent<Projectile>();
-
-
-
 			// If this Enemy is off screen, don't damage it.
-
-			if (!bndCheck.isOnScreen)
-			{                                // c
-
+			if (!bndCheck.isOnScreen) { // c
 				Destroy(otherGO);
-
 				break;
-
 			}
-
-
 
 			// Hurt this Enemy
-
 			ShowDamage();
-
 			// Get the damage amount from the Main WEAP_DICT.
-
 			health -= Main.GetWeaponDefinition(p.type).damageOnHit;
-
-			if (health <= 0)
-			{                                           // d
-
+			if (health <= 0) { // d                                   
+				// Tell the Main singleton that this ship was destroyed // b
+				if (!notifiedOfDestruction) {
+					Main.S.shipDestroyed(this);
+				}
+				notifiedOfDestruction = true;
 				// Destroy this Enemy
-
 				Destroy(this.gameObject);
-
 			}
-
-			Destroy(otherGO);                                          // e
-
+			Destroy(otherGO); // e
 			break;
-
-
-
 		default:
-
 			print("Enemy hit by non-ProjectileHero: " + otherGO.name); // f
-
 			break;
-
-
-
 		}
-
 	}
 
-
-	void ShowDamage()
-	{                                                      // e
-
-		foreach (Material m in materials)
-		{
-
+	void ShowDamage() { // e
+		foreach (Material m in materials) {
 			m.color = Color.red;
-
 		}
-
 		showingDamage = true;
-
 		damageDoneTime = Time.time + showDamageDuration;
-
 	}
 
-
-
-	void UnShowDamage()
-	{                                                    // f
-
-		for (int i = 0; i < materials.Length; i++)
-		{
-
+	void UnShowDamage() { // f
+		for (int i = 0; i < materials.Length; i++) {
 			materials[i].color = originalColors[i];
-
 		}
-
 		showingDamage = false;
-
 	}
-
 }
--- Assets/__Scripts/Hero.cs
+++ Assets/__Scripts/Hero.cs
@@ -1,239 +1,138 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
-public class Hero : MonoBehaviour
-{
+public class Hero : MonoBehaviour {
+	static public Hero		S;
 
-	static public Hero S; // Singleton                               // a
-
-
-
-	[Header("Set in Inspector")]
-
-	// These fields control the movement of the ship
-
-	public float speed = 30;
-
-	public float rollMult = -45;
-
-	public float pitchMult = 30;
-
+	[Header("Set in inspector")]
+	public float	speed = 30;
+	public float	rollMult = -45;
+	public float  	pitchMult=30;
 	public float gameRestartDelay = 2f;
-
 	public GameObject projectilePrefab;
-
 	public float projectileSpeed = 40;
+	public Weapon[] weapons; // a
 
-
-	[Header("Set Dynamically")]
-
+	[Header("Set dynamically")]
 	[SerializeField]
+	private float	_shieldLevel=1;
+	// This variable holds a reference to the last triggering GameObject
+	private GameObject lastTriggerGo = null; // a
 
-	private float _shieldLevel = 1;
-
-	private GameObject lastTriggerGo = null;
-
-
-	public delegate void WeaponFireDelegate();                               // a
-
-
+	// Declare a new delegate type WeaponFireDelegate
+	public delegate void WeaponFireDelegate(); // a
 	// Create a WeaponFireDelegate field named fireDelegate.
-
-
 	public WeaponFireDelegate fireDelegate;
 
+	void Start() {
+		S = this;
+		// fireDelegate += TempFire; // b
 
-
-	void Awake()
-	{
-
-		if (S == null)
-		{
-
-
-			S = this; // Set the Singleton                                   // a
-
-		}
-		else
-		{
-
-			Debug.LogError("Hero.Awake() - Attempted to assign second Hero.S!");
-
-		}
-
-		fireDelegate += TempFire;
-
-
+		// Reset the weapons to start _Hero with 1 blaster
+		ClearWeapons();
+		weapons[0].SetType(WeaponType.blaster);
 	}
 
-
-
-	void Update()
-	{
-
-		// Pull in information from the Input class
-
-		float xAxis = Input.GetAxis("Horizontal");                            // b
-
-		float yAxis = Input.GetAxis("Vertical");                              // b
-
-
-
-		// Change transform.position based on the axes
+	// Update is called once per frame
+	void Update () {
+		float xAxis = Input.GetAxis("Horizontal");
+		float yAxis = Input.GetAxis("Vertical");
 
 		Vector3 pos = transform.position;
-
 		pos.x += xAxis * speed * Time.deltaTime;
-
 		pos.y += yAxis * speed * Time.deltaTime;
-
 		transform.position = pos;
 
+		// rotate the ship to make it feel more dynamic
+		transform.rotation = Quaternion.Euler(yAxis*pitchMult, xAxis*rollMult, 0);
 
-
-		// Rotate the ship to make it feel more dynamic                      // c
-
-		transform.rotation = Quaternion.Euler(yAxis * pitchMult, xAxis * rollMult, 0);
-
-		// if (Input.GetKeyDown(KeyCode.Space))
-		//{                           // a
-
-		//  TempFire();
-
-		//}
-
-		if (Input.GetAxis("Jump") == 1 && fireDelegate != null)
-		{            // d
-
-
-			fireDelegate();                                                  // e
-
-
+		// Allow the ship to fire
+		// if ( Input.GetKeyDown(KeyCode.Space) ) { // c
+		// TempFire(); // c
+		// } // c
+		// Use the fireDelegate to fire Weapons
+		// First, make sure the button is pressed: Axis("Jump")
+		// Then ensure that fireDelegate isn't null to avoid an error
+		if (Input.GetAxis("Jump") == 1 && fireDelegate != null) { // d
+			fireDelegate(); // e
 		}
-
-
 	}
 
-
-
-	void TempFire()
-	{                                                        // b
-
-		GameObject projGO = Instantiate<GameObject>(projectilePrefab);
-
-		projGO.transform.position = transform.position;
-
-		Rigidbody rigidB = projGO.GetComponent<Rigidbody>();
-
-		//rigidB.velocity = Vector3.up * projectileSpeed;
-
-		Projectile proj = projGO.GetComponent<Projectile>();                 // h
-
-
-		proj.type = WeaponType.blaster;
-
-
-		float tSpeed = Main.GetWeaponDefinition(proj.type).velocity;
-
-
-		rigidB.velocity = Vector3.up * tSpeed;
-
-
-
-
-	}
-
-	void OnTriggerEnter(Collider other)
-	{
-
-
+	void OnTriggerEnter(Collider other) {
 		Transform rootT = other.gameObject.transform.root;
-
 		GameObject go = rootT.gameObject;
+		//print("Triggered: "+go.name); // b
 
-		//print("Triggered: " + go.name);
-
-		if (go == lastTriggerGo)
-		{                                           // c
-
+		// Make sure it's not the same triggering go as last time
+		if (go == lastTriggerGo) { // c
 			return;
-
 		}
+		lastTriggerGo = go; // d
 
-		lastTriggerGo = go;                                                  // d
-
-
-
-		if (go.tag == "Enemy")
-		{  // If the shield was triggered by an enemy
-
-			shieldLevel--;        // Decrease the level of the shield by 1
-
-			Destroy(go);          // … and Destroy the enemy                 // e
-
+		if (go.tag == "Enemy") {
+			// If the shield was triggered by an enemy
+			// Decrease the level of the shield by 1
+			shieldLevel--;
+			// Destroy the enemy
+			Destroy(go); 
 		} else if (go.tag == "PowerUp") {
-
 			// If the shield was triggered by a PowerUp
-
 			AbsorbPowerUp(go);
 		} else {
-
-			print("Triggered by non-Enemy: " + go.name);                       // f
-
+			print( "Triggered by non-Enemy: "+ go.name); // f
 		}
-
 	}
 
-	public void AbsorbPowerUp( GameObject go ) {
-
+	public void AbsorbPowerUp(GameObject go) {
 		PowerUp pu = go.GetComponent<PowerUp>();
-
 		switch (pu.type) {
+		case WeaponType.shield: // a
+			shieldLevel++;
+			break;
 
-
-
-		// Leave this switch block empty for now.
-
-
-
-		}
-
-		pu.AbsorbedBy( this.gameObject );
-
-	}
-
-
-
-	public float shieldLevel
-	{
-
-		get
-		{
-
-			return (_shieldLevel);                                          // a
-
-		}
-
-		set
-		{
-
-			_shieldLevel = Mathf.Min(value, 4);                             // b
-
-			// If the shield is going to be set to less than zero
-
-			if (value < 0)
-			{                                                 // c
-
-				Destroy(this.gameObject);
-
-				Main.S.DelayedRestart(gameRestartDelay);
-
-
+		default: // b
+			if (pu.type == weapons[0].type) { // If it is the same type // c
+				Weapon w = GetEmptyWeaponSlot();
+				if (w != null) {
+					// Set it to pu.type
+					w.SetType(pu.type);
+				}
+			} else { // If this is a different weapon type // d
+				ClearWeapons();
+				weapons[0].SetType(pu.type);
 			}
-
+			break;
 		}
-
+		pu.AbsorbedBy(this.gameObject);
 	}
 
+	public float shieldLevel {
+		get {
+			return(_shieldLevel); // a
+		}
+		set {
+			_shieldLevel = Mathf.Min (value, 4); // b
+			// If the shield is going to be set to less than zero
+			if (value < 0) { // c
+				Destroy (this.gameObject);
+				// Tell Main.S to restart the game after a delay
+				Main.S.DelayedRestart( gameRestartDelay ); // a
+			}
+		}
+	}
+	Weapon GetEmptyWeaponSlot() {
+		for (int i = 0; i < weapons.Length; i++) {
+			if (weapons[i].type == WeaponType.none) {
+				return (weapons[i]);
+			}
+		}
+		return (null);
+	}
+
+	void ClearWeapons() {
+		foreach (Weapon w in weapons) {
+			w.SetType(WeaponType.none);
+		}
+	}
 }
